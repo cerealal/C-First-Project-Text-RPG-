8303fbbeb820ededbf9652b3db2946f0135bf883
Alejandro Fernandez <33691537+cerealal@users.noreply.github.com>
2020-01-21 22:03:20 -0500

    Add files via upload

---
+++ Base c++ Project/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+string a;
+cout << "are you a little bitch? [Yes or No]" << endl;
+cin >> a;
+if (a == "yes")
+{
+    cout << "that's what I thought, little bitch" << endl;
+} else if(a == "no")
+{
+    cout << "oh? so you say you aren't a bitch? What makes you think this?" << endl;
+} 
+else { cout << "that doesn't answer my question fuckwad";}
+
+return 0;
+
+
+
+
+}
